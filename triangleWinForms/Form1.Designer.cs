2f5e8deb17753d318bcdc9c61829c1222d269b82
User <User@DESKTOP-PQL9IG2>
2023-10-24 10:55:53 +0300

    Добавьте файлы проекта.

---
+++ triangleWinForms/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace triangleWinForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(207, 175);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(197, 22);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(207, 233);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(197, 22);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            this.textBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox2_KeyPress);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(207, 293);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(195, 22);
+            this.textBox3.TabIndex = 2;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            this.textBox3.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox3_KeyPress);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(89, 175);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(112, 33);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "side 1 ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(89, 233);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 29);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "side 2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(89, 293);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(78, 29);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "side 3";
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(59, 44);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(478, 108);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Enter the lengths of the sides of a triangle in the boxes below. Use the same len" +
+    "gth units for all sides.";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox4.Location = new System.Drawing.Point(74, 356);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(463, 142);
+            this.textBox4.TabIndex = 7;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(468, 203);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(131, 52);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "analyse";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(842, 564);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.MaximumSize = new System.Drawing.Size(860, 611);
+            this.MinimumSize = new System.Drawing.Size(860, 611);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ triangleWinForms/Form1.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics.Eventing.Reader;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace triangleWinForms
+{
+    public partial class Form1 : Form
+    {
+        double x, y, z;
+        string sx, sy, sz, result;
+        bool b,b2,b3;
+        
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            sz = textBox3.Text;
+            b3 = double.TryParse(sz, out z);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            double sumxy, sumxz, sumyz;
+            bool exists;
+            if (x != 0)
+            {
+                if (y != 0)
+                {
+                    if (z != 0)
+                    {
+                        sumxy = x + y;
+                        sumxz = x + z;
+                        sumyz = y + z;
+                        if ((sumxy > z) || (sumxz > y))
+                        {
+                            exists = true;
+                        }
+                        else
+                        {
+                            if (sumyz > x)
+                            {
+                                exists = true;
+                            }
+                            else
+                            {
+                                exists = false;
+                                textBox4.Text = "Треугольник с такими длинами сторон не существует";
+                            }
+                        }
+                        if (exists)
+                        {
+                            if (x == y)
+                            {
+                                if (x == z)
+                                {
+                                    textBox4.Text = "Треугольник равносторонний";
+                                }
+                                else
+                                {
+                                    textBox4.Text = "Треугольник равнобедренный";
+                                }
+
+                            }
+                            else
+                            {
+                                if (x == z) textBox4.Text = "Треугольник равнобедренный";
+                                else
+                                {
+                                    if (y == z) textBox4.Text = "Треугольник равнобедренный";
+                                    else
+                                    {
+                                        textBox4.Text = "Треугольник разносторонний";
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    else textBox4.Text = "Треугольника не существует";
+
+                }
+                else textBox4.Text = "Треугольника не существует";
+
+            }
+            else textBox4.Text = "Треугольника не существует";
+            
+        }
+
+        private void textBox3_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (Char.IsDigit(e.KeyChar)) return;
+            if (e.KeyChar == '.') e.KeyChar = '.';
+            if (e.KeyChar == ',')
+            {
+                if (textBox3.Text.Contains(","))
+                    e.Handled = true;
+                if (textBox3.Text == "")
+                    e.Handled = true;
+                return;
+            }
+            if (e.KeyChar == 0)
+            {
+                if (textBox3.Text == "")
+                    e.Handled = true;
+                return;
+            }
+            if (e.KeyChar == 8) return;
+            e.Handled = true;
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            sy = textBox2.Text;
+            b2 = double.TryParse(sy, out y);
+        }
+
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (Char.IsDigit(e.KeyChar)) return;
+            if (e.KeyChar == '.') e.KeyChar = '.';
+            if (e.KeyChar == ',')
+            {
+                if (textBox2.Text.Contains(","))
+                    e.Handled = true;
+                if (textBox2.Text == "")
+                    e.Handled = true;
+                return;
+            }
+            if (e.KeyChar == 0)
+            {
+                if (textBox2.Text == "")
+                    e.Handled = true;
+                return;
+            }
+            if (e.KeyChar == 8) return;
+            e.Handled = true;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            sx = textBox1.Text;
+            b = double.TryParse(sx, out x);
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (Char.IsDigit(e.KeyChar)) return;
+            if (e.KeyChar=='.') e.KeyChar= '.';
+            if (e.KeyChar == ',')
+            {
+                if (textBox1.Text.Contains(","))
+                    e.Handled = true;
+                if (textBox1.Text == "")
+                    e.Handled = true;
+                return;
+            }
+            if (e.KeyChar == 0)
+            {
+                if (textBox1.Text == "")
+                    e.Handled = true;
+                return;
+            }
+            if (e.KeyChar == 8) return;
+            e.Handled = true;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        
+
+        }
+    }
+}
